28e82b18d49767a112d5e2a1ef23b06eb7280b43
edelnorchannel-art <edelnorchannel@gmail.com>
2026-07-01 15:36:45 +0300

    Update watermark_service.py

--- services/watermark_service.py
+++ services/watermark_service.py
@@ -16,12 +16,28 @@ logger = logging.getLogger(__name__)
 
 WATERMARK_PATH = Path(__file__).resolve().parents[1] / "imgs" / "watermark.png"
 WATERMARK_HEIGHT_RATIO = 1 / 8
+WATERMARK_WIDTH_RATIO_VERTICAL = 1 / 3
 WATERMARK_MARGIN = 20
 JPEG_QUALITY = 90
 
 
 class WatermarkError(Exception):
     pass
+
+
+def _resize_logo(logo: Image.Image, photo: Image.Image) -> Image.Image:
+    is_vertical = photo.height > photo.width
+
+    if is_vertical:
+        logo_width = max(1, int(photo.width * WATERMARK_WIDTH_RATIO_VERTICAL))
+        ratio = logo_width / logo.width
+        logo_height = max(1, int(logo.height * ratio))
+    else:
+        logo_height = max(1, int(photo.height * WATERMARK_HEIGHT_RATIO))
+        ratio = logo_height / logo.height
+        logo_width = max(1, int(logo.width * ratio))
+
+    return logo.resize((logo_width, logo_height), Image.Resampling.LANCZOS)
 
 
 def apply_watermark(source_path: Path, destination_path: Path) -> None:
@@ -35,10 +51,8 @@ def apply_watermark(source_path: Path, destination_path: Path) -> None:
 
             with Image.open(WATERMARK_PATH) as logo:
                 logo = logo.convert("RGBA")
-                logo_height = max(1, int(photo.height * WATERMARK_HEIGHT_RATIO))
-                ratio = logo_height / logo.height
-                logo_width = max(1, int(logo.width * ratio))
-                logo = logo.resize((logo_width, logo_height), Image.Resampling.LANCZOS)
+                logo = _resize_logo(logo, photo)
+                logo_height = logo.height
 
                 position = (
                     WATERMARK_MARGIN,
